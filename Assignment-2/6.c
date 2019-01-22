dfe0640806ca3bd05399872006e2cb800b5a3cc7
gaurabdg <gaurabdg0@gmail.com>
2019-01-22 09:42:57 +0530

    A2: solved 6

--- Assignment-2/6.c
+++ Assignment-2/6.c
@@ -9,8 +9,47 @@
 #define f2(i,x,y) for(int i=x;i<=y;i++)
 #define null NULL
 
-int g[1000][1000];
+int g[1000][1000], vis[1000]={0};
 int n, npr;
+
+void addEdge(int g[n][n], int i, int j)
+{
+    g[i][j]=1;
+    g[j][i]=1;
+}
+
+void dfs(int gpr[npr][npr], int x)
+{
+	vis[x]=1;
+	for(int i=0;i<npr;i++)
+		if(gpr[x][i]==1&&vis[i]!=1)
+			dfs(gpr, i);
+}
+
+bool isArticulationPoint(int gpr[npr][npr], int u)
+{
+	int components=0;
+	for(int i=0;i<npr;i++)
+	{
+		if(vis[i]==0)
+		{
+			dfs(gpr, i);
+			components++;	
+			printf("break points: %d\n",i );		
+		}
+	}
+
+	for(int i=0;i<npr;i++)
+		vis[i]=0;
+	if(u==0)
+		components-=1;
+	if(components > 1){
+		printf("#components: %d\n",components);
+		return true;
+	}
+	return false;
+
+}
 
 int main()
 {
@@ -39,15 +78,14 @@ int main()
         }
 
     npr=k;
-
-    int gpr[npr][npr];
-    memset(gpr,0,sizeof(gpr));
-
-    // f1(i,0,n) 
+    // npr = n;
+    //     int gpr[npr][npr];
+    // memset(gpr,0,sizeof(gpr));
+    // f1(i,0,p)
     // {
-    //     f1(j,0,n) 
-    //         printf("%d ", g[i][j]);
-    //     printf("\n");
+    //     int x,y;
+    //     scanf("%d %d", &x, &y);
+    //     addEdge(gpr, x,y);
     // }
 
     f1(i,0,n)
@@ -60,12 +98,33 @@ int main()
                         gpr[g[i][k]][g[i][j]] =1;
                     }
 
+    int mem[npr];
+    memset(mem,0,sizeof(mem));
 
-    // f1(i,0,npr) 
-    // {
-    //     f1(j,0,npr) 
-    //         printf("%d", gpr[i][j]);
-    //     printf("\n");
-    // }
+    for(int i=0;i<npr;i++)
+	{
+		for(int j=0;j<npr;j++)
+		{
+			if(gpr[i][j]==1)
+			{
+				mem[j]=1;
+				gpr[i][j]=0;
+			}
+		}
+
+		if(isArticulationPoint(gpr, i))
+			printf("answer: %d\n",i);
+
+		for(int k=0;k<npr;k++)
+		{
+			if(mem[k]==1)
+			{
+				gpr[i][k]=1;
+				mem[k]=0;
+			}
+		}
+
+	}
 
 }
+
