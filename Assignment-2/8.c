3bc427236e515185cea4d15535009346fb900efd
gaurabdg <gaurabdg0@gmail.com>
2019-01-31 18:39:09 +0530

    final commit

--- Assignment-2/8.c
+++ Assignment-2/8.c
@@ -1,137 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
-#include<limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-#define d(x) printf("%d",x);
-#define f1(i,x,y) for(int i=x;i<y;i++)
-#define f2(i,x,y) for(int i=x;i<=y;i++)
-#define null NULL
-#define qs 10
-
-int g[100][100], uni[100], vis[100], dist[100], parent[100], n,cnt, result[100];
-
-typedef struct Node{
-    int val;
-    struct Node* next;
-}node;
-
-typedef struct Queue{
-    node* front;
-    node* rear;
-    int size;   
-}q;
-
-node* createNode(int x)
-{
-    node* t = (node*)malloc(sizeof(node));
-    t->val = x;
-    t->next = NULL;
-    return t;
-}
-
-q* createQ()
-{
-    q* t = (q*)malloc(sizeof(q));
-    t->rear = null;
-    t->front = null;
-    return t;
-}
-
-void enq(q* qu, int x)
-{
-    node* t = createNode(x);
-    if(qu->rear == null)
-    {
-        qu->front = qu->rear = t;
-        return;
+void bfs(int n, int arr[n][n], int dest[n], int v) {
+    int queue[n], visited[n], head = 0, tail = 0;
+    for (int i = 0; i < n; ++i) {
+        visited[i] = 0;
+        dest[i] = INT_MAX;
     }
-    qu->rear->next = t;
-    qu->rear = t;
-}
 
-node* deq(q* q)
-{
-    if(q->front == null)
-        return null;
-    
-    node* t = q->front;
-    q->front = q->front->next;
+    visited[v] = 1;
+    dest[v] = 0;
+    queue[head] = v;
 
-    if(q->front==null)
-        q->rear = null;
-    return t;
-}
-
-bool isEmpty(q* q)
-{
-    if(q->rear==null)
-        return true;
-    return false;
-}
-
-void bfs(q* q, int s, int t)
-{
-    memset(vis,0,sizeof(vis));
-    memset(parent,-1,sizeof(parent));
-    f1(i,0,n)
-        dist[i]=INT_MAX;
-    vis[s] = 1;
-    dist[s] = 0;
-    enq(q,s);
-
-    while(!isEmpty(q))
-    {
-        s = deq(q)->val;
-        f1(i,0,n)
-        {
-            if(g[s][i])
-            {
-                if(!vis[i])
-                {
-                    vis[i] = 1;
-                    dist[i] = dist[s]+1;
-                    if(dist[i]==t){
-                        printf("%d ",i);
-                        cnt+=1;
-                    }
-                    enq(q,i);
-                }
+    while (head <= tail) {
+        int u = queue[head++];
+        for (int i = 0; i < n; ++i) {
+            if (arr[u][i] && !visited[i]) {
+                visited[i] = 1;
+                dest[i] = dest[u] + 1;
+                queue[++tail] = i;
             }
         }
     }
 }
 
-void addEdge(int u, int v)
-{
-    g[u][v] = 1;
-    g[v][u] = 1;
-}
-
 int main()
 {
-    cnt=0;
-    q* w = createQ();
-    int t;
-    scanf("%d", &n);
-    memset(g,0,sizeof(g));
-    memset(result,0,sizeof(result));
-    // f2(i,1,n)
-    // {
-    //     int u,v;
-    //     scanf("%d %d",&u,&v);
-    //     g[u][v] = 1;
-    //     g[v][u] = 1;
-    // }
-    scanf("%d",&t);
-    // d(t)
-    addEdge(0,1);
-    addEdge(1,2);
-    addEdge(2,3);
-    addEdge(1,4);
+    int n, m, t, count = 0;
+    scanf("%d%d", &n, &m);
+    int arr[n][n], dest[n];
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            arr[i][j] = 0;
+        }
+    }
 
-    f1(i,0,n)
-        bfs(w, i,t);
-    printf("%d\n", cnt);
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        arr[u][v] = arr[v][u] = 1;
+    }
+    scanf("%d", &t);
+
+    for (int i = 0; i < n; ++i) {
+        bfs(n, arr, dest, i);
+        for (int j = 0; j < n; ++j) {
+            if (dest[j] == t)
+                count++;
+        }
+    }
+
+    printf("%d", count / 2);
 }
