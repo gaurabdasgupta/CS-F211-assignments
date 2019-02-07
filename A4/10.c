fec7ae87ace4a85a93bdd057a39f5dbe4765c62b
gaurabdg <gaurabdg0@gmail.com>
2019-02-07 16:25:40 +0530

    A4: solved 10

---
+++ A4/10.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+#define s(x) scanf("%d", &x)
+#define p(x) printf("%d\n", x)
+#define f(i,x,y) for(int i=x;i<y;i++)
+
+int main()
+{
+    int n;
+    s(n);
+    int a[n], res=0;
+
+    f(i,0,n)
+    {
+        int x;
+        s(x);
+        res^=x;
+    }
+
+    p(res);
+
+}
